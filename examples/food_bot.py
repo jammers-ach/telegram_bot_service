9eeaae76393267792bf9a10b79239960bde27ad1
jammers <james@mathews.fi>
2025-05-01 11:14:49 +0200

    added weekly stats for food

--- examples/food_bot.py
+++ examples/food_bot.py
@@ -186,6 +186,18 @@ class FoodBot(TelegramBot):
         except Exception as e:
             await update.message.reply_text(str(e))
 
+
+    @TelegramBot.command
+    async def stats(self, update):
+        chat_id = update.message.chat_id
+        msg = self.generate_top_list(chat_id)
+        await update.message.reply_markdown("In the last 7 days you ate:")
+        await update.message.reply_markdown(msg)
+        msg = self.generate_new_food(chat_id)
+        await update.message.reply_markdown("New foods for you in the last 7 days:")
+        await update.message.reply_markdown(msg)
+
+
     async def handle_update(self, update):
         item = update.message.text
         await self.log_date(update, item, datetime.datetime.now())
@@ -201,20 +213,6 @@ class FoodBot(TelegramBot):
         await update.message.reply_markdown(f"{day}: *{item}* at `{time}`")
 
 
-
-    async def today(self):
-        """prints out today's food"""
-        date = datetime.datetime.today()
-        day = date.strftime("%Y-%m-%d")
-        keys = self.db[str(self.chat_ids[0])].get(day,None)
-        if keys:
-            await self.single_send_msg("Today you ate:", chat_ids=self.chat_ids)
-            await self.single_send_msg(self.print_day(keys), chat_ids=self.chat_ids, markdown=True)
-        else:
-            await self.single_send_msg("Today you ate nothing!?!", chat_ids=self.chat_ids)
-            await self.single_send_msg("maybe you should correct that?", chat_ids=self.chat_ids)
-
-
     def print_day(self, day):
         text = ""
         day.sort(key=lambda x: x[0])
@@ -223,16 +221,99 @@ class FoodBot(TelegramBot):
         return text
 
 
+    def _top_items(self, chat_id, top):
+        '''returns the top N items from this week'''
+        db = self.db[str(chat_id)]
+        food_count_week = {}
+        dates = [(datetime.datetime.today() - datetime.timedelta(days=i)).date().isoformat() for i in range(6, -1, -1)]
+        for d in dates:
+            keys = db.get(d, [])
+            for _, key in keys:
+                if key not in food_count_week:
+                    food_count_week[key] = 0
+                food_count_week[key] += 1
+        sorted_items = sorted(food_count_week.items(), key=lambda item: -1*item[1])
+        return sorted_items[0:top]
+
+
+    def generate_top_list(self, chat_id, top=10):
+        '''Generates a list of the top 5 foods you ate in the week
+        '''
+        top_items = self._top_items(chat_id, top=top)
+
+        text = ""
+        for item, count in top_items:
+            text += f"`{count:>2}:` {item}\n"
+
+        return text
+
+    def generate_new_food(self, chat_id, days=7):
+        '''
+        prints out which foods were new for you in the last N days
+        '''
+        dates = [(datetime.datetime.today() - datetime.timedelta(days=i)).date().isoformat() for i in range(days-1, -1, -1)]
+        db = self.db[str(chat_id)]
+        other_foods = []
+        weeks_foods = []
+        other_foods, weeks_foods = [], []
+        for key, items in db.items():
+            (other_foods if key not in dates else weeks_foods).extend(food for _, food in items)
+
+        other_foods = set(other_foods)
+        weeks_foods = set(weeks_foods)
+        new_foods = (weeks_foods - other_foods)
+
+        msg = ""
+        for food in new_foods:
+            msg += f"{food}\n"
+        return msg
+
+
+    async def weekly_stats(self):
+        """Gives the weekly stats"""
+        for chat_id in self.chat_ids:
+            await self._weekly_stats(chat_id)
+
+    async def _weekly_stats(self, chat_id):
+        msg = self.generate_top_list(chat_id)
+        await self.single_send_msg("In the last 7 days you ate:", chat_ids=[chat_id], markdown=True)
+        await self.single_send_msg(msg, chat_ids=[chat_id], markdown=True)
+
+        msg = self.generate_new_food(chat_id)
+        await self.single_send_msg("New foods for you in the last 7 days:", chat_ids=[chat_id], markdown=True)
+        await self.single_send_msg(msg, chat_ids=[chat_id], markdown=True)
+
+    async def today(self):
+        """prints out today's food"""
+        date = datetime.datetime.today()
+        day = date.strftime("%Y-%m-%d")
+        for chat_id in self.chat_ids:
+            try:
+                keys = self.db[str(chat_id)].get(day,None)
+                print(chat_id)
+                if keys:
+                    await self.single_send_msg("Today you ate:", chat_ids=[chat_id,])
+                    await self.single_send_msg(self.print_day(keys), chat_ids=[chat_id,], markdown=True)
+                else:
+                    await self.single_send_msg("Today you ate nothing!?!", chat_ids=[chat_id,])
+                    await self.single_send_msg("maybe you should correct that?", chat_ids=[chat_id,])
+            except Exception as e:
+                print(e)
+
+
 
 def run():
     parser = argparse.ArgumentParser(description="Foodbot: Logs what you eat each day")
     parser.add_argument('-today', action='store_true', help='Prints out what was eaten today')
+    parser.add_argument('-week', action='store_true', help='Prints out the weekly stats')
     args = parser.parse_args()
 
     bot = FoodBot()
 
     if args.today:
         asyncio.run(bot.today())
+    elif args.week:
+        asyncio.run(bot.weekly_stats())
     else:
         bot.start()
 
